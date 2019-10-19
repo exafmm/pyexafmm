d36462ff7f2badeb347cda34f9576def3f168de0
Srinath Kailasa <srinathkailasa@gmail.com>
2019-10-19 11:11:26 +0100

    Remove private import

--- fmm/utils.py
+++ fmm/utils.py
@@ -1,5 +1,5 @@
 """Utility routines."""
-import time as _time
+import time 
 
 class Timer:
     """Context manager to measure times."""
@@ -11,10 +11,10 @@ class Timer:
         self.interval = 0
 
     def __enter__(self):
-        self.start = _time.time()
+        self.start = time.time()
         return self
 
     def __exit__(self, *args):
-        self.end = _time.time()
+        self.end = time.time()
         self.interval = self.end - self.start
 
