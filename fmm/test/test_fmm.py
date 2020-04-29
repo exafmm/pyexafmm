96f8bb7196092011b5fb7dfd823ee4d7422d3d17
Srinath Kailasa <srinathkailasa@gmail.com>
2020-04-29 11:18:20 +0100

    Add p2m test

--- fmm/test/test_fmm.py
+++ fmm/test/test_fmm.py
@@ -6,7 +6,7 @@ import pytest
 
 import fmm.hilbert as hilbert
 from fmm.octree import Octree
-from fmm.fmm import Fmm
+from fmm.fmm import Fmm, cartesian_to_spherical, direct_calculation, p2m
 
 NPOINTS = 1000
 MAXIMUM_LEVEL = 3
@@ -33,3 +33,18 @@ def test_upward_and_donward_pass(octree):
 
     for index in range(NPOINTS):
         assert len(fmm._result_data[index]) == NPOINTS
+
+
+def test_p2m(octree):
+    """Test Multipole expansion"""
+    sources = octree._sources
+    sources_sph_harm = np.apply_along_axis(cartesian_to_spherical, 1, sources)
+
+    # well separated
+    target = np.array([10, 10, 10])
+    target_sph_harm = cartesian_to_spherical(target)
+
+    direct_result = direct_calculation(sources, target)
+    p2m_result = p2m(sources_sph_harm, target_sph_harm, 1)
+
+    assert np.around(direct_result) == np.around(p2m_result)
