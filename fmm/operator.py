FIX: Check operator pre-computation script (#27)
* Update gitignore

* More scrutiny of surface creation

* Add p2p results to test data generation

* Remove redundant downward pass test

fmm/operator.py
@@ -174,7 +174,10 @@ def compute_pseudo_inverse(matrix, alpha=None):
 
 
 def compute_check_to_equivalent_inverse(
-        kernel_function, upward_check_surface, upward_equivalent_surface
+        kernel_function,
+        upward_check_surface,
+        upward_equivalent_surface,
+        alpha=None
         ):
     """
     Compute the inverse of the upward check-to-equivalent gram matrix, and the
@@ -186,6 +189,8 @@ def compute_check_to_equivalent_inverse(
     kernel_function : function
     upward_check_surface : np.array(shape=(n, 3))
     upward_equivalent_surface : np.array(shape=(n, 3))
+    alpha : float [optional]
+        Regularisation parameter
 
     Returns:
     --------
@@ -201,7 +206,11 @@ def compute_check_to_equivalent_inverse(
     )
 
     # Compute SVD of Gram Matrix
-    uc2e_v, uc2e_u, dc2e_v, dc2e_u = compute_pseudo_inverse(c2e)
+
+    if alpha is None:
+        uc2e_v, uc2e_u, dc2e_v, dc2e_u = compute_pseudo_inverse(c2e)
+    else:
+        uc2e_v, uc2e_u, dc2e_v, dc2e_u = compute_pseudo_inverse(c2e, alpha)
 
     return (uc2e_v, uc2e_u, dc2e_v, dc2e_u)
 

fmm/test/test_fmm.py
@@ -1,15 +1,45 @@
 """
 Test the FMM
 """
+import os
+import pathlib
+import subprocess
+
 import numpy as np
 import pytest
 
 from fmm.fmm import Fmm
 from fmm.operator import scale_surface, p2p
+import utils.data as data
 
 
-def test_upward_pass():
-    fmm = Fmm(config_filename='config.json')
+HERE = pathlib.Path(os.path.dirname(os.path.abspath(__file__)))
+CONFIG_FILEPATH = HERE.parent.parent / "test_config.json"
+CONFIG = data.load_json(CONFIG_FILEPATH)
+
+ORDER = CONFIG['order']
+
+OPERATOR_DIRPATH = HERE.parent.parent / f'precomputed_operators_order_{ORDER}'
+SCRIPT_DIRPATH = HERE.parent.parent / 'scripts'
+
+
+def setup_module(module):
+    os.chdir(HERE.parent)
+    subprocess.run(['python', SCRIPT_DIRPATH / 'precompute_operators.py', CONFIG_FILEPATH])
+    os.chdir('test')
+
+
+def teardown_module(module):
+    os.chdir(HERE.parent.parent)
+    subprocess.run(['rm', '-fr', f'precomputed_operators_order_{ORDER}'])
+
+
+@pytest.fixture
+def fmm():
+    return Fmm(config_filename='test_config.json')
+
+
+def test_upward_pass(fmm):
     fmm.upward_pass()
 
     root_equivalent_surface = scale_surface(
@@ -32,32 +62,4 @@ def test_upward_pass():
         fmm.source_densities
     )
 
-    assert np.isclose(direct_fmm.density, direct_particles.density, rtol=0.05)
-
-    print(direct_fmm.density, direct_particles.density)
-    assert False
-
-
-# def test_downward_pass():
-
-#     fmm = Fmm(config_filename='config.json')
-
-#     fmm.upward_pass()
-#     fmm.downward_pass()
-
-#     fmm_results = np.array([res.density[0] for res in fmm.result_data])
-
-#     direct = p2p(
-#         kernel_function=fmm.kernel_function,
-#         targets=fmm.targets,
-#         sources=fmm.sources,
-#         source_densities=fmm.source_densities
-#     ).density
-
-#     error = 100*abs(fmm_results - direct)/direct
-#     print(f'mean percentage error {sum(error)/len(fmm_results)}')
-#     print()
-#     print("first 10 direct results", direct[:10])
-#     print("first 10 fmm results", fmm_results[:10])
-
-#     assert False
+    assert np.isclose(direct_fmm.density, direct_particles.density, rtol=0.01)

scripts/precompute_operators.py
@@ -106,7 +106,8 @@ def main(
         uc2e_v, uc2e_u, dc2e_v, dc2e_u = compute_check_to_equivalent_inverse(
             kernel_function=kernel_function,
             upward_check_surface=upward_check_surface,
-            upward_equivalent_surface=upward_equivalent_surface
+            upward_equivalent_surface=upward_equivalent_surface,
+            alpha=0
         )
 
         # Save matrices
@@ -155,8 +156,8 @@ def main(
 
             pc2ce = gram_matrix(
                 kernel_function=kernel_function,
-                sources=parent_upward_check_surface,
-                targets=child_upward_equivalent_surface,
+                targets=parent_upward_check_surface,
+                sources=child_upward_equivalent_surface,
             )
 
             # Compute M2M operator for this octant
@@ -203,8 +204,8 @@ def main(
 
         for source_idx, source_key in enumerate(interaction_list):
 
-            source_4d_idx = fmm.hilbert.get_4d_index_from_key(source_key)
-            source_relative_to_target = source_4d_idx[:3] - center_4d_index[:3]
+            source_4d_index = fmm.hilbert.get_4d_index_from_key(source_key)
+            source_relative_to_target = source_4d_index[:3] - center_4d_index[:3]
             magnitude = np.linalg.norm(source_relative_to_target)
 
             sources_relative_to_targets[source_idx][:3] = source_relative_to_target
@@ -216,26 +217,33 @@ def main(
         for idx, source_to_target_vec in enumerate(sources_relative_to_targets):
             print(f'Computed ({idx+1}/{loading}) M2L operators')
 
-            source_key = int(source_to_target_vec[-1])
+            source_key = int(source_to_target_vec[4])
             source_center = fmm.hilbert.get_center_from_key(source_key, x0, r0)
-            source_level = target_level
             source_upward_equivalent_surface = scale_surface(
-                surface, r0, source_level, source_center, alpha_inner
+                    surface=surface,
+                    radius=r0,
+                    level=source_level,
+                    center=source_center,
+                    alpha=alpha_inner
                 )
 
             target_center = fmm.hilbert.get_center_from_key(center_key, x0, r0)
 
             # Compute target check surface
             target_downward_check_surface = scale_surface(
-                surface, r0, target_level, target_center, alpha_inner
+                    surface=surface,
+                    radius=r0,
+                    level=target_level,
+                    center=target_center,
+                    alpha=alpha_inner
             )
 
             scale = (1/2)**(target_level)
 
             se2tc = gram_matrix(
                 kernel_function=kernel_function,
-                sources=target_downward_check_surface,
-                targets=source_upward_equivalent_surface,
+                sources=source_upward_equivalent_surface,
+                targets=target_downward_check_surface,
             )
 
             tmp = np.matmul(dc2e_u, se2tc)

scripts/test/test_precompute_operators.py
@@ -42,8 +42,6 @@ def octree():
     sources = data.load_hdf5_to_array('random_sources', 'random_sources', '../../data')
     targets = data.load_hdf5_to_array('random_sources', 'random_sources', '../../data')
 
-    print("HWERW", sources.shape)
-
     source_densities = data.load_hdf5_to_array(
         'source_densities', 'source_densities', '../../data')
 
@@ -81,7 +79,7 @@ def npoints():
 def test_m2m(npoints, octree, m2m):
 
     parent_key = 0
-    child_key = 1
+    child_key = fmm.hilbert.get_children(parent_key)[0]
 
     x0 = octree.center
     r0 = octree.radius
@@ -104,13 +102,13 @@ def test_m2m(npoints, octree, m2m):
     parent_direct = p2p(KERNEL_FUNCTION, distant_point, parent_equivalent_surface, parent_equivalent_density)
     child_direct = p2p(KERNEL_FUNCTION, distant_point, child_equivalent_surface, child_equivalent_density)
 
-    assert np.isclose(parent_direct.density, child_direct.density, rtol=0.01)
+    assert np.isclose(parent_direct.density, child_direct.density, rtol=0.05)
 
 
 def test_l2l(npoints, octree, l2l):
 
     parent_key = 0
-    child_key = 2
+    child_key = fmm.hilbert.get_children(parent_key)[0]
 
     x0 = octree.center
     r0 = octree.radius
@@ -135,7 +133,8 @@ def test_l2l(npoints, octree, l2l):
     parent_direct = p2p(KERNEL_FUNCTION, local_point, parent_equivalent_surface, parent_equivalent_density)
     child_direct = p2p(KERNEL_FUNCTION, local_point, child_equivalent_surface, child_equivalent_density)
 
-    assert np.isclose(parent_direct.density, child_direct.density, rtol=0.01)
+    assert np.isclose(parent_direct.density, child_direct.density, rtol=0.05)
+
 
 def test_m2l(
     npoints,
@@ -181,8 +180,8 @@ def test_m2l(
     source_direct = p2p(
         kernel_function=KERNEL_FUNCTION,
         targets=local_point,
-        source_densities=source_equivalent_density,
-        sources=source_equivalent_surface
+        sources=source_equivalent_surface,
+        source_densities=source_equivalent_density
     )
 
     assert np.isclose(target_direct.density, source_direct.density, rtol=0.01)