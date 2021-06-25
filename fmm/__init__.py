0caeef183cac5fd9c719b1373345f3f04266cdf0
Srinath Kailasa <srinathkailasa@gmail.com>
2021-06-25 14:12:50 +0100

    ENH: Add version info to top level import (#90)

--- fmm/__init__.py
+++ fmm/__init__.py
@@ -1,4 +1,9 @@
-"""
-API
-"""
+# API
 from fmm.fmm import Fmm
+
+# Info
+import fmm.__version__ as _version
+
+__version__ = _version.__version__
+__description__ = _version.__description__
+__title__ = _version.__title__
